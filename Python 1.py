8eefff1033b8d01c6793c17d0ccefc22417007c3
mateushne <mateushn002@gmail.com>
2026-03-25 22:25:08 -0300

    Add files via upload

---
+++ Python 1.py
@@ -0,0 +1,11 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> nome= inpunt('Qualé o seu nome? ')
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    nome= inpunt('Qualé o seu nome? ')
+NameError: name 'inpunt' is not defined. Did you mean: 'input'?
+>>> nome =input ('Qual é o seu nome? ')
+Qual é o seu nome? Mateus
+>>> print('Olá,Mateus,!' 'Prazer em te conhececer')
+Olá,Mateus,!Prazer em te conhececer
